5d70e1f86de343b824b821ce9f7315d3d3648ef3
Habteweld Yimer <ht@Habtewelds-MacBook-Pro.local>
2023-12-26 23:16:52 +0300

    inital commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+from keras import datasets, layers, models
+
+(training_images, training_labels), (testing_images, testing_labels) = datasets.cifar10.load_data() 
+training_images, testing_images = training_images / 255, testing_images / 255
+
+class_names = ["Plane", "Car", "Bird", "Cat", "Deer", "Dog", "Frog", "Horse", "Ship", "Truck"]
+
+model = models.load_model("image_classifier.model")
+
+img = cv.imread("car.jpg")
+img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+
+plt.imshow(img, cmap=plt.cm.binary)
+
+prediction = model.predict(np.array([img]) / 255)
+
+index = np.argmax(prediction)
+
+print(f"Prediction: {class_names[index]}")
+
+plt.show()
